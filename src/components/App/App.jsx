cc7c5d175d53fdaf3be61b95a0b66e42981fcc9d
Serhii Ivanchenko <serhiiivanchenkotan@gmail.com>
2024-05-30 15:55:33 +0300

    add Loader component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,19 +3,29 @@ import './App.css';
 import getPhotos from '../unsplash-api-fetch';
 import SearchBar from '../SearchBar/SearchBar';
 import ImageGallery from '../ImageGallery/ImageGallery';
+import Loader from '../Loader/Loader';
 
 function App() {
   const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSearch = async query => {
-    console.log(query);
-    const data = await getPhotos(query);
-    setImages(data);
+    try {
+      setIsLoading(true);
+      setImages([]);
+      console.log(query);
+      const data = await getPhotos(query);
+      setImages(data);
+      setIsLoading(false);
+    } catch (error) {
+    } finally {
+    }
   };
 
   return (
     <>
       <SearchBar onSearch={handleSearch} />
+      {isLoading && <Loader />}
       {images.length > 0 && <ImageGallery items={images} />}
     </>
   );
